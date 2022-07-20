4dc32cc7194598dd6cd01773eb5310b2954b7628
Behruz <87355568+XqwertyB@users.noreply.github.com>
2022-07-20 18:07:39 +0500

    static collect

--- appmenagment/settings.py
+++ appmenagment/settings.py
@@ -190,16 +190,16 @@ TATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ] # new
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # new
-# STATIC_URL = '/static/'
-# if DEBUG:
-#    STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static'),
-#    ]
-# else:
-#    STATIC_ROOT = os.path.join(BASE_DIR,'static')
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
