bdc0d59d15aeef65a6ce16a40e6b61da4528ee0b
Behruz <87355568+XqwertyB@users.noreply.github.com>
2022-07-20 18:36:19 +0500

    debug

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn config.wsgi --log-file -
+web: gunicorn appmenagment.wsgi --log-file -
--- appmenagment/settings.py
+++ appmenagment/settings.py
@@ -196,7 +196,7 @@ if DEBUG:
    os.path.join(BASE_DIR, 'static'),
    ]
 else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
