6e56afe888b7e76569305101d0b1b030b71eafec
Behruz <87355568+XqwertyB@users.noreply.github.com>
2022-07-31 20:40:30 +0500

    Update settings.py

--- appmenagment/settings.py
+++ appmenagment/settings.py
@@ -96,22 +96,22 @@ SITE_ID = 1
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'employe',
-#         'USER': 'admin',
-#         'PASSWORD': '124252',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5433',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbabpqhvquhn65',
+        'USER': 'axzurofimeakin',
+        'PASSWORD': 'f1490d4820243881dde0f102083e222816de8c45b3c490293737edef2fb8ba5c',
+        'HOST': 'ec2-3-213-228-206.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
