00a2bc06a411de3c43fd426ca9d0f67185d87217
Behruz <87355568+XqwertyB@users.noreply.github.com>
2022-08-04 18:34:34 +0500

    Update admin.py

--- person/admin.py
+++ person/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Typ, Task, Taskcontroll, Person, Images, usertyp
+from .models import Typ, Task,  Person, Images, usertyp
 from group_order.models import group_emp, costumer, order, order_detail
 
 admin.site.register(Typ)
 admin.site.register(Task)
-admin.site.register(Taskcontroll)
 admin.site.register(usertyp)
 admin.site.register(Person)
 admin.site.register(Images)
